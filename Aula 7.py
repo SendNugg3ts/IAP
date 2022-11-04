3c5e5ba6741521279a8652de059e5bf65c982c9e
Luis Silva <luis.fmsilva@hotmail.com>
2022-11-04 10:40:20 +0000

    update

--- Aula 7.py
+++ Aula 7.py
@@ -1,3 +1,5 @@
+from matplotlib import pyplot as plt
+
 d = {
     "a":1,
     "b":2,
@@ -12,3 +14,10 @@ dicionario_para_listas(d)
 chaves , valores = dicionario_para_listas(d)
 print(chaves)
 print(valores)
+#Ex 2
+def desenha_grafico_barras(keys,values):
+    
+    plt.bar(keys,values)
+    return plt.show()
+desenha_grafico_barras(chaves,valores)
+
