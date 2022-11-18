b3ae9703e51793c6b7ba3311a6a148efb658e128
SendNugg3ts <luis.fmsilva@hotmail.com>
2022-11-18 10:53:34 +0000

    aula9

--- Aula_8.py
+++ Aula_8.py
@@ -2,34 +2,52 @@
 import sys
 import json
 
+
+def fun_ajuda():
+    print("Como escrever: ")
+    print("  python3 Aula_08.py [local] [dia]")
+    print("  O local é o distrito de portugal pertendido")
+    print("  A Data é o dia da previsão")
+
+
+args = sys.argv[1:]
+if args[0]== "ajuda":
+    fun_ajuda()
+    exit()
+
+elif len(args)== 2:
+    local = args[0]
+    data=int(args[1])
+    if data < 1 or data > 5:
+        print("Data must be between 1-5")
+
+elif len(args) == 1:
+    local = args[0]
+
+elif args == []:
+    print("Erro: não foi introduzido um local.")
+    exit()
+
+
 def get_code(cidade):
     dicc = json.load(open("previsoes/locais.json"))
     return dicc[cidade]
 
 
-def get_predict(code):
+def get_predict(code,dia):
     dicc_file = json.load(open(f"previsoes/{code}.json"))
-    return dicc_file["data"]
+    return dicc_file["data"][dia -1]
         
 
-args = sys.argv[1:]
-if args == []:
-    print("Erro: não foi introduzido um local.")
-    exit()
-    
-local = args[0]
-
 codigo = get_code(local)
-previsoes_dict = get_predict(codigo)
+previsoes_dict = get_predict(codigo,data)
 pretty_dic=json.dumps(previsoes_dict,indent =1)
-print(previsoes_dict)
-for arg in previsoes_dict:
-    print("_______________________________")
-    print(f"Data: {arg['forecastDate']}")
-    print("_______________________________")
-    print(f"{arg['tMax']}")
-    print(arg["tMin"])
-    print(arg["precipitaProb"])
-
+#print(pretty_dic)
+print("_______________________________")
+print(f"Data: {previsoes_dict['forecastDate']}")
+print("_______________________________")
+print(f"Temperatura Maxima: {previsoes_dict['tMax']}")
+print(f"Temperatura Minima {previsoes_dict['tMin']}")
+print(f"Precipitação: {previsoes_dict['precipitaProb']}")
 
 
